25b54226739419d792e91c04200ff61f419f0953
NatashaGwena <tashagwena@gmail.com>
2022-06-10 15:21:18 +0300

    registration-app

--- src/RegistrationComponent.js
+++ src/RegistrationComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
 class RegistrationComponent extends Component{
-
+  
     constructor(props){
         super(props)
         this.state={
